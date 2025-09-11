16b92043a7e4a1b7da53f38d9be778e8bfab9de9
MAGS <120091142+MAGS-GH@users.noreply.github.com>
2025-09-11 08:00:36 +0200

    Add online course planning docs and update GuessANumber
    Added Plan.md and Teams-besked.txt for online course planning in Obsidian/Valgfag/PlanForOnline. Updated GuessANumber.cs to generate and display a random number for guessing. Changed ActiveDirectoryTesting debug profile settings.

--- GF2/Projekter/Konsol/Hjemmet/GuessANumber.cs
+++ GF2/Projekter/Konsol/Hjemmet/GuessANumber.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Hjemmet
 {
     public class GuessANumber
@@ -5,6 +7,14 @@ namespace Hjemmet
         public void Start()
         {
             Console.WriteLine("Gæt et tal er ikke implementeret endnu.");
+
+            // Random Number
+
+            Random random = new Random();
+            int numberToGuess = random.Next(1, 1000000000);
+
+            Console.WriteLine($"Psssst, du skal gætte {numberToGuess}");
+
             Console.ReadKey();
         }
     }
