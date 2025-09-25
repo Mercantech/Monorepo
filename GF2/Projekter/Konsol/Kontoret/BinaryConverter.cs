ab9f309c785cb339b08e2299599cea4a57d861f6
MAGS <120091142+MAGS-GH@users.noreply.github.com>
2025-09-25 11:21:10 +0200

    vault backup: 2025-09-25 11:21

--- GF2/Projekter/Konsol/Kontoret/BinaryConverter.cs
+++ GF2/Projekter/Konsol/Kontoret/BinaryConverter.cs
@@ -4,6 +4,37 @@ namespace Kontoret
     {
         public void Start()
         {
+            Console.WriteLine("Skriv en octet ind - altså et tal fra 0-255");
+            Console.ReadLine();
+            int bit1 = 0;
+            int num = 173;
+            if (128 > num)
+            {
+                bit1 = 0;
+            }
+            else
+            {
+                bit1 = 1;
+                num = num - 128;
+            }
+            if (64 > num)
+            {
+                bit1 = 0;
+            }
+            else
+            {
+                bit1 = 1;
+                num = num - 64;
+            }
+            if (128 > num)
+            {
+                bit1 = 0;
+            }
+            else
+            {
+                bit1 = 1;
+                num = num - 128;
+            }
             Console.WriteLine("Binærkodeomformer er ikke implementeret endnu.");
             Console.ReadKey();
         }
