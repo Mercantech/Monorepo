00517ba9f56d12d591305b7fd5683dc8f78c5be4
MAGS <120091142+MAGS-GH@users.noreply.github.com>
2025-09-25 10:10:42 +0200

    vault backup: 2025-09-25 10:10

--- GF2/Opgaver/5.Arrays.cs
+++ GF2/Opgaver/5.Arrays.cs
@@ -35,9 +35,6 @@ namespace Opgaver
             );
             // Lav opgaven herunder!
             string[] navne = new string[5];
-            navne[0] = "Anders";
-            navne[1] = "Jóse";
-            navne[2] = "Simon";
          }
 
         public static void Array2()
